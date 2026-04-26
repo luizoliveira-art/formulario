7a7d3e129cb6d5ef0de19feb51e5646e0901df8c
luizoliveira-art <luiz.oliveira@aluno.ufca.edu.br>
2026-04-26 18:31:14 -0300

    Add files via upload

---
+++ formulario.py
@@ -0,0 +1,34 @@
+print('Digite seu Nome')
+Nome = str(input('Nome: '))
+print(Nome)
+
+print('Digite seu Sobrenome')
+Sobrenome = str(input('Sobrenome: '))
+print(Sobrenome)
+
+print('Digite seu Email')
+Email = str(input('Email: '))
+print(Email)
+
+print('Digite sua Idade')
+idade = str(input('Idade: '))
+print(idade)
+
+print('Digite sua Altura')
+Altura = str(input('Altura: '))
+print(Altura)
+
+print('Digite seu Peso')
+Peso = str(input('Peso: '))
+print(Peso)
+
+print('Digite seu Sexo')
+Sexo = str(input('Sexo: '))
+print(Sexo)
+
+print(f'{Nome} {Sobrenome}')
+print(Email)
+print(idade)
+print(Altura)
+print(Peso)
+print(Sexo)
